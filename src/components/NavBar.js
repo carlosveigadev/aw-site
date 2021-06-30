c1cb2eb380da6a5c4a71580dfc53966d9d105d3d
wrack <carlosveiga1618@gmail.com>
2021-06-30 14:56:36 -0300

    add drawer to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,24 @@
 import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
+  Drawer,
+  Box,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
   Button,
+  useDisclosure,
 } from '@chakra-ui/react';
+import { useRef } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { useHistory } from 'react-router';
 import ThemeChanger from './ThemeChanger';
 
 const NavBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const history = useHistory();
+  const btnRef = useRef();
+
   const goHome = () => {
     history.push('/');
   };
@@ -20,20 +27,34 @@ const NavBar = () => {
     history.push('/dashboard');
   };
   return (
-    <Menu>
-      <MenuButton as={Button}>
-        <GiHamburgerMenu />
-      </MenuButton>
-      <MenuList
-        display="flex"
-        justifyContent="center"
-        flexDirection="column"
+    <Box>
+      <Button
+        p="1em"
+        ref={btnRef}
+        variant="ghost"
+        onClick={onOpen}
+        display={{ sm: 'flex', xl: 'none' }}
+        fontSize={{ sm: '68px' }}
       >
-        <MenuItem onClick={goHome} display="flex" justifyContent="center">Home</MenuItem>
-        <MenuItem onClick={goDashboard} display="flex" justifyContent="center">Dashboard</MenuItem>
-        <ThemeChanger />
-      </MenuList>
-    </Menu>
+        <GiHamburgerMenu />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody my="4em" display="flex" flexDirection="column">
+            <Button variant="ghost" onClick={goHome}>Home</Button>
+            <Button variant="ghost" onClick={goDashboard}>Dashboard</Button>
+            <ThemeChanger />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
   );
 };
 
