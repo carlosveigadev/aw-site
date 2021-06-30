d5d988c4d20ec5fdc8918b42223c134b0174db7a
wrack <carlosveiga1618@gmail.com>
2021-06-30 15:19:04 -0300

    add proper navigation to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import {
   Drawer,
   Box,
@@ -7,12 +8,33 @@ import {
   DrawerCloseButton,
   Button,
   useDisclosure,
+  Text,
+  Flex,
 } from '@chakra-ui/react';
 import { useRef } from 'react';
+import PropTypes from 'prop-types';
+
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { useHistory } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import ThemeChanger from './ThemeChanger';
+
+const MenuItem = ({
+  children, to = '/', ...rest
+}) => (
+  <NavLink
+    exact
+    to={to}
+    activeStyle={{
+      textDecoration: 'underline',
+    }}
+  >
+    <Text display="inline" {...rest}>
+      {children}
+    </Text>
+  </NavLink>
+);
 
 const NavBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -26,36 +48,52 @@ const NavBar = () => {
   const goDashboard = () => {
     history.push('/dashboard');
   };
+
   return (
-    <Box>
-      <Button
-        p="1em"
-        ref={btnRef}
-        variant="ghost"
-        onClick={onOpen}
-        display={{ sm: 'flex', xl: 'none' }}
-        fontSize={{ sm: '68px' }}
+    <>
+      <Box>
+        <Button
+          p="1em"
+          ref={btnRef}
+          variant="ghost"
+          onClick={onOpen}
+          display={{ sm: 'flex', xl: 'none' }}
+          fontSize={{ sm: '68px' }}
+        >
+          <GiHamburgerMenu />
+        </Button>
+        <Drawer
+          isOpen={isOpen}
+          placement="left"
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerBody my="4em" display="flex" flexDirection="column">
+              <Button variant="ghost" onClick={goHome}>Home</Button>
+              <Button variant="ghost" onClick={goDashboard}>Dashboard</Button>
+              <ThemeChanger />
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+      <Flex
+        py="1em"
+        display={{ base: 'none', xl: 'flex' }}
+        justifyContent="space-around"
       >
-        <GiHamburgerMenu />
-      </Button>
-      <Drawer
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerBody my="4em" display="flex" flexDirection="column">
-            <Button variant="ghost" onClick={goHome}>Home</Button>
-            <Button variant="ghost" onClick={goDashboard}>Dashboard</Button>
-            <ThemeChanger />
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </Box>
+        <MenuItem to="/">Home</MenuItem>
+        <MenuItem to="/dashboard">Dashboard</MenuItem>
+      </Flex>
+    </>
   );
+};
+
+MenuItem.propTypes = {
+  children: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
 };
 
 export default NavBar;
